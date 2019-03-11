0175c9f8bbc50f3efe8a8486472249f26a65d333
Ay0mikun <>
2019-03-10 23:22:10 -0700

    First commit with Codestar

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Cry in the Dojo, laugh on the Battlefield',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
